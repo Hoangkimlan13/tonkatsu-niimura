3e50b8da619a88c64492cea40ae307786b12bb04
Hoangkimlan13 <hoangkimlan13@gmail.com>
2026-07-16 00:30:36 +0900

    fix loi 1

--- app/data.ts
+++ app/data.ts
@@ -1,6 +1,5 @@
 export { getNavItems, navItems } from "./data/navigation";
 export { menuPreviewItems, menuSections } from "./data/menu";
 export { news } from "./data/news";
-export { extendedSitemap } from "./data/sitemap";
 export { stores } from "./data/stores";
 export { languageOptions } from "./i18n";
